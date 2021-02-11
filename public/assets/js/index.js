0609660b64c69c3bfd33cd75b6b251f75db58813
Luba Pecheneva <lp5786766@gmail.com>
2021-02-11 14:51:02 -0500

    my words are set up properly

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
+    $('select').formSelect();
   });
+
+  // This page will display:
+
+  // - Navbar (My Books, My languages)
+
+  // - Add a new word form (word, sentence, book, additional details)
+
+  // - My recent words as cards (with collapsible info)
+
+  // - list of books/languages
