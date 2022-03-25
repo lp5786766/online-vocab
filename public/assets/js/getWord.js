8d803be2e72ced3e8c46aac4c410c49cc9d04887
Luba Pecheneva <lp5786766@gmail.com>
2022-03-25 18:36:15 -0400

    delete reacts outside getwords

--- public/assets/js/getWord.js
+++ public/assets/js/getWord.js
@@ -1,5 +1,3 @@
-$(document).ready(() => {
-
 const wordOutput = $('.my-words');
 const partSpeechOutput = $('.part-of-speech');
 const definitionOutput = $('.definition');
@@ -18,14 +16,10 @@ const authorInput = $('#author-input');
 
 // My words elements
 const myCardsArea = $('.word-cards');
+const deleteBtn = $('.delete');
 
-// Materialize js
-$('.collapsible').collapsible();
-$('select').formSelect();
-$('.tooltipped').tooltip();
-
-let words = [];
 const getWords = () => {
+  let words = [];
   $.get('/api/words', function (data) {
     // console.log('Words', data);
     words = data;
@@ -39,126 +33,113 @@ const getWords = () => {
       console.log(i, word.word);
       const wordCard = $(
         `<ul class="collapsible col s4">
-        <li>
-          <div class="collapsible-header">
-            <p class="my-words" name="word">${word.word}</p>
-            <p class="part-of-speech">${word.partSpeech}</p>
-          </div>
-          <div class="collapsible-body" style>
-            <p class="definition">
-              ${word.definition}
-            </p>
-            <b class="sentence"
-              >"${word.sentence}"</b
-            >
-            <p class="book">
-              ${word.book} <i class="author">by ${word.author}</i>
-            </p>
-            <a
-              class="waves-effect waves-light btn-small tooltipped light-green"
-              data-position="bottom"
-              data-tooltip="Edit"
-              ><i class="material-icons">edit</i></a
-            >
-            <a
-              class="waves-effect waves-light btn-small tooltipped red lighten-1 delete"
-              id="delete-${word.id}"
-              data-position="bottom"
-              data-tooltip="Delete"
-              ><i class="material-icons">delete_forever</i></a
-            >
-          </div>
-        </li>
-      </ul>`
+      <li>
+        <div class="collapsible-header">
+          <p class="my-words" name="word">${word.word}</p>
+          <p class="part-of-speech">${word.partSpeech}</p>
+        </div>
+        <div class="collapsible-body" style>
+          <p class="definition">
+            ${word.definition}
+          </p>
+          <b class="sentence"
+            >"${word.sentence}"</b
+          >
+          <p class="book">
+            ${word.book} <i class="author">by ${word.author}</i>
+          </p>
+          <a
+            class="waves-effect waves-light btn-small tooltipped light-green"
+            data-position="bottom"
+            data-tooltip="Edit"
+            ><i class="material-icons">edit</i></a
+          >
+          <a
+            class="waves-effect waves-light btn-small tooltipped red lighten-1 delete"
+            id="delete-${word.id}"
+            data-position="bottom"
+            data-tooltip="Delete"
+            ><i class="material-icons">delete_forever</i></a
+          >
+        </div>
+      </li>
+    </ul>`
       );
-  
+
       myCardsArea.prepend(wordCard);
       $('.collapsible').collapsible();
       $('.tooltipped').tooltip();
 
-      
-
-      // Delete word function
-      const deleteBtn = $('.delete');
-      const deleteWord = () => {
-
-        alert(`clicked ${word.id}`);
-      };
-      deleteBtn.on('click', deleteWord);
-      
-   
+      // DELETE word function
+      // const deleteWord = () => {
+      //   alert(`clicked`);
+      // };
     });
-    
   });
 };
+$(document).ready(() => {
+  // Materialize js
+  $('.collapsible').collapsible();
+  $('select').formSelect();
+  $('.tooltipped').tooltip();
+
   // This function grabs words from the database and updates the view
-  
   getWords();
 
-  
+  // Generate and display words from the db
 
-  addBtn.click((event) => {
-    // prevent reloading while building, delete later
-    event.preventDefault();
-    insertWord();
-  });
-  
- 
+  // GET all words
 
- 
-// Generate and display words from the db
+  $('.tooltipped').tooltip();
 
+  // POST a new word
+  // TODO: present a quick confirmation form on 'add' click before saving the word to make sure there are no mistakes
+  const insertWord = () => {
+    const word = {
+      word: newWordInput.val().trim(),
+      partSpeech: partSpeechInput.val(),
+      definition: definitionInput.val().trim(),
+      book: bookInput.val().trim(),
+      author: authorInput.val().trim(),
+      sentence: sentenceInput.val().trim(),
+    };
+    $.post('/api/words', word, getWords);
+  };
 
-// GET all words
+  // This function inserts a new todo into our database and then updates the view
+  //  const saveWord = (event) => {
+  //   event.preventDefault();
+  //   const word = {
+  //     text: $newItemInput.val().trim(),
+  //     complete: false
+  //   };
 
+  //   $.post("/api/words", todo, getTodos);
+  //   $newItemInput.val("");
+  // }
 
+  // This page will display:
 
-$('.tooltipped').tooltip();
+  // - Navbar (My Books, My languages) potentially the whole page is reflecting only one language
 
+  // - Add a new word form (word, sentence, book, additional details)
 
+  // - My recent words as cards (with collapsible info)
+  // API Call to Back End:
 
-// POST a new word
-// TODO: present a quick confirmation form on 'add' click before saving the word to make sure there are no mistakes
-const insertWord = () => {
-  const word = {
-    word: newWordInput.val().trim(),
-    partSpeech: partSpeechInput.val(),
-    definition: definitionInput.val().trim(),
-    book: bookInput.val().trim(),
-    author: authorInput.val().trim(),
-    sentence: sentenceInput.val().trim(),
-  }
-  $.post("/api/words", word, getWords);
-}
+  // HERE we can start by reading the words that are in the database already and create new cards for them
+  // ADD EDIT FUNCTION
 
+  // - list of books/languages
+});
 
+addBtn.click((event) => {
+  // prevent reloading while building, delete later
+  // event.preventDefault();
+  // insertWord();
+  alert('add clicked');
+});
 
-
-
-// This function inserts a new todo into our database and then updates the view
-//  const saveWord = (event) => {
-//   event.preventDefault();
-//   const word = {
-//     text: $newItemInput.val().trim(),
-//     complete: false
-//   };
-
-//   $.post("/api/words", todo, getTodos);
-//   $newItemInput.val("");
-// }
-
-// This page will display:
-
-// - Navbar (My Books, My languages) potentially the whole page is reflecting only one language
-
-// - Add a new word form (word, sentence, book, additional details)
-
-// - My recent words as cards (with collapsible info)
-// API Call to Back End:
-
-// HERE we can start by reading the words that are in the database already and create new cards for them
-// ADD EDIT FUNCTION
-
-// - list of books/languages
-
+$(document).on('click', 'a.delete', () => {
+  alert(`delete ${this.id} clicked`); // jQuery 1.4.3+
 });
